20fb8c04fc62bdcfac50c49fda5de01d487bd2bd
Xeenych <Xeenych@gmail.com>
2020-11-19 21:16:03 +0300

    123

--- debug_print.h
+++ debug_print.h
@@ -1,8 +1,9 @@
 #ifdef DEBUG_PRINT
+#include <stdio.h>
 #define DEBUG 1
 #else
 #define DEBUG 0
 #endif
 
 #define debug_print(...) \
-do { if (DEBUG) UNUSED(printf(__VA_ARGS__)); } while (0)
+do { if (DEBUG) printf(__VA_ARGS__); } while (0)
