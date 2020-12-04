43709fe3337b7a8bd7661eb129076d31950f2664
Xeenych <Xeenych@gmail.com>
2020-12-04 21:00:37 +0300

    1

--- IBuffer.hpp
+++ IBuffer.hpp
@@ -9,7 +9,7 @@ namespace ZOQ {
 	public:
 		inline oBuffer(void* data, size_t size);
 		inline size_t read(void* buf, size_t len);
-		inline void advance(size_t len);
+		inline size_t advance(size_t len);
 		inline void* data() const;
 		inline size_t data_len() const;
 	private:
@@ -22,11 +22,12 @@ namespace ZOQ {
 		data_ptr((uint8_t*)data), size(sz) {
 	}
 
-	void oBuffer::advance(size_t len) {
+	size_t oBuffer::advance(size_t len) {
 		if (len > size)
 			len = size;
 		size -= len;
 		data_ptr += len;
+		return len;
 	}
 
 	void* oBuffer::data() const{
@@ -45,7 +46,8 @@ namespace ZOQ {
 		return to_read;
 	}
 
-	inline size_t operator>>(oBuffer& b, IWrite& w) {
+	template <typename oB, typename iW>
+	inline size_t operator>>(oB& b, iW& w) {
 		auto data = b.data();
 		auto data_len = b.data_len();
 		auto written = w.write(data, data_len);
