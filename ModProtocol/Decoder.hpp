9c2ba2d74f6be0f622243a45925818f251b206f2
Xeenych <xeenych@gmail.com>
2022-09-28 11:21:26 +0300

    foramtting

--- ModProtocol/Decoder.hpp
+++ ModProtocol/Decoder.hpp
@@ -1,98 +1,92 @@
 #pragma once
-#include <stdint.h>
-#include <stdlib.h>
+#include <cstdint>
+#include <cstdlib>
+
 #include "common.hpp"
 
 namespace ZOQ::ModProtocol {
 
-
 class Decoder {
-public:
-	class IDecoderCallback {
-	public:
-		virtual void Execute(uint8_t const* buf, size_t len) = 0;
-	};
+  public:
+    class IDecoderCallback {
+      public:
+        virtual void Execute(uint8_t const* buf, size_t len) = 0;
+    };
 
+    using callback_t = void (*)(uint8_t const* buf, size_t len);
 
-using callback_t = void (*)(uint8_t const* buf, size_t len);
+  public:
+    Decoder(uint8_t* buf, size_t size, IDecoderCallback& on_finish);
+    void Decode(uint8_t const* chunk, size_t chunksize);  // возвращает количество отбработанных символов
 
-public:
-	Decoder(uint8_t* buf, size_t size, IDecoderCallback& on_finish);
-	void Decode(uint8_t const* chunk, size_t chunksize);	// возвращает количество отбработанных символов
-
-private:
-	inline void reset();
-	inline void decode_symbol(uint8_t byte);
-	uint8_t* const buf_start;
-	size_t const buf_size;
-	uint8_t lrc = LRCSTART;
-	bool is_high = true;
-	uint8_t* decoded_ptr;
-	IDecoderCallback& OnFinish;	// Колбэк, который вызывается при успешном декодировании посылки
-	uint8_t current;
+  private:
+    inline void reset();
+    inline void decode_symbol(uint8_t byte);
+    uint8_t* const buf_start;
+    size_t const buf_size;
+    uint8_t lrc = LRCSTART;
+    bool is_high = true;
+    uint8_t* decoded_ptr;
+    IDecoderCallback& OnFinish;  // Колбэк, который вызывается при успешном декодировании посылки
+    uint8_t current;
 };
 
-inline Decoder::Decoder (uint8_t* _buf, size_t _size, IDecoderCallback& on_finish) :
-	buf_start(_buf),
-	buf_size(_size),
-	lrc(LRCSTART),
-	is_high(true),
-	decoded_ptr(_buf),
-	OnFinish(on_finish) {
+inline Decoder::Decoder(uint8_t* _buf, size_t _size, IDecoderCallback& on_finish)
+    : buf_start(_buf), buf_size(_size), lrc(LRCSTART), is_high(true), decoded_ptr(_buf), OnFinish(on_finish)
+{
 }
 
-inline void Decoder::decode_symbol(uint8_t symbol) {
+inline void Decoder::decode_symbol(uint8_t symbol)
+{
+    if (symbol == FSTART) {
+        reset();
+        return;
+    }
 
-	if (symbol == FSTART) {
-		reset();
-		return;
-	}
+    if (symbol == FEND) {
+        if (lrc == 0) {  // Приняли последний символ и сошлась контрольная сумма
+            size_t decoded_len = decoded_ptr - buf_start - 1;  // длина посылки без контрольной суммы
+            OnFinish.Execute(buf_start, decoded_len);
+        }
+        reset();
+        return;
+    }
 
-	if ( symbol == FEND ) {
-		if ( lrc == 0 ) {	// Приняли последний символ и сошлась контрольная сумма
-			size_t decoded_len = decoded_ptr - buf_start - 1; // длина посылки без контрольной суммы
-			OnFinish.Execute(buf_start, decoded_len);
-		}
-		reset();
-		return;
-	}
+    uint8_t decoded = CodeToByte(symbol);
+    if (decoded == 0xff) {  // Приняли неизвестный символ
+        reset();
+        return;
+    }
 
-	uint8_t decoded = CodeToByte(symbol);
-	if (decoded == 0xff) {	// Приняли неизвестный символ
-		reset();
-		return;
-	}
-
-	if (is_high) {
-		current = decoded << 4;
-		is_high = false;
-	}
-	else {
-		current = current | decoded;
-		is_high = true;
-		lrc -= current;
-		if (decoded_ptr < buf_start + buf_size) {	// Если декодированная посылка помещается в буфер
-			*decoded_ptr = current;
-			decoded_ptr++;
-		} else {
-			reset();	// Декодированная посылка не умещается в буфер
-		}
-	}
+    if (is_high) {
+        current = decoded << 4;
+        is_high = false;
+    } else {
+        current = current | decoded;
+        is_high = true;
+        lrc -= current;
+        if (decoded_ptr < buf_start + buf_size) {  // Если декодированная посылка помещается в буфер
+            *decoded_ptr = current;
+            decoded_ptr++;
+        } else {
+            reset();  // Декодированная посылка не умещается в буфер
+        }
+    }
 }
 
-inline void Decoder::Decode(uint8_t const* chunk, size_t chunksize) {
-
-	while (chunksize > 0) {
-		decode_symbol(*chunk++);
-		chunksize--;
-	}
+inline void Decoder::Decode(uint8_t const* chunk, size_t chunksize)
+{
+    while (chunksize > 0) {
+        decode_symbol(*chunk++);
+        chunksize--;
+    }
 }
 
-inline void Decoder::reset() {
-	decoded_ptr = buf_start;
-	lrc = LRCSTART;
-	is_high = true;
+inline void Decoder::reset()
+{
+    decoded_ptr = buf_start;
+    lrc = LRCSTART;
+    is_high = true;
 }
 
-
-} // namespace
+}  // namespace ZOQ::ModProtocol
--- hal_uart_it.hpp
+++ hal_uart_it.hpp
@@ -1,31 +1,31 @@
 #pragma once
 #include "zoq.hpp"
 
-
 namespace ZOQ::Stm32_HAL {
 
-	class hal_uart_it : public IReadWrite {
-	public:
-		hal_uart_it(UART_HandleTypeDef* h);
-		size_t write(void *pData, size_t size) override; // возвращает количество переданных байт
-		size_t read(void *pData, size_t size) override;
-	private:
-		UART_HandleTypeDef* const handle;
-		CyclicBufferT<uint8_t, 100> rx_buf;
-		CyclicBufferT<uint8_t, 100> tx_buf;
-		uint8_t tmp_rx;
-		uint8_t tmp_tx;
+class hal_uart_it : public IReadWrite {
+  public:
+    hal_uart_it(UART_HandleTypeDef* h);
+    size_t write(void* pData, size_t size) override;  // возвращает количество переданных байт
+    size_t read(void* pData, size_t size) override;
 
-		static hal_uart_it* select_instance(UART_HandleTypeDef const* h);
+  private:
+    UART_HandleTypeDef* const handle;
+    CyclicBufferT<uint8_t, 100> rx_buf;
+    CyclicBufferT<uint8_t, 100> tx_buf;
+    uint8_t tmp_rx;
+    uint8_t tmp_tx;
 
-		void OnRxCplt();
-		void OnTxCplt();
-		void OnError();
-		void enable_irq();
-		void disable_irq();
+    static hal_uart_it* select_instance(UART_HandleTypeDef const* h);
 
-		friend void ::HAL_UART_TxCpltCallback(UART_HandleTypeDef *huart);
-		friend void ::HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart);
-		friend void ::HAL_UART_ErrorCallback(UART_HandleTypeDef *huart);
-	};
-}
+    void OnRxCplt();
+    void OnTxCplt();
+    void OnError();
+    void enable_irq();
+    void disable_irq();
+
+    friend void ::HAL_UART_TxCpltCallback(UART_HandleTypeDef* huart);
+    friend void ::HAL_UART_RxCpltCallback(UART_HandleTypeDef* huart);
+    friend void ::HAL_UART_ErrorCallback(UART_HandleTypeDef* huart);
+};
+}  // namespace ZOQ::Stm32_HAL
