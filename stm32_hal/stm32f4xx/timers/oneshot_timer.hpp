9d27da2b9cc8d6ff4ecc23d9ce2933b930c6437a
Xeenych <xeenych@gmail.com>
2023-09-17 22:47:49 +0300

    async timer

---
+++ stm32_hal/stm32f4xx/timers/oneshot_timer.hpp
@@ -0,0 +1,13 @@
+#pragma once
+#include "stm32f4xx_hal.h"
+
+namespace ZOQ::stm32_hal::stm32f4xx::timers {
+
+class oneshot_timer_t {
+  public:
+    oneshot_timer_t() = default;
+
+  private:
+};
+
+}  // namespace ZOQ::stm32_hal::stm32f4xx::timers
