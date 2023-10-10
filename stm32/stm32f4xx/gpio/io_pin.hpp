b6de71585df3a35d693f6a148736e67b4e278e50
Xeenych <xeenych@gmail.com>
2023-10-10 13:30:06 +0300

    1

--- stm32/stm32f4xx/gpio/io_pin.hpp
+++ stm32/stm32f4xx/gpio/io_pin.hpp
@@ -10,7 +10,6 @@ using namespace ZOQ::itf;
 class io_pin_t final : public io_pin_itf {
   public:
     constexpr io_pin_t(const pin_def_t& p) : _port(static_cast<GPIO_TypeDef*>(p.port)), _pin(p.pin){};
-    // constexpr io_pin_t(GPIO_TypeDef* port, const uint32_t& pin) : _port(port), _pin(pin){};
     void set() override { _port->BSRR = _pin; }
     void reset() override { _port->BSRR = (uint32_t)(_pin << 16U); }
     bool get() override { return ((_port->IDR & _pin) != (uint32_t)GPIO_PIN_RESET); }
