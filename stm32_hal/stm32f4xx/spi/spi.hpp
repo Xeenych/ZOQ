db8c805584c2f9716e9677d6faefafb12ca122cc
Xeenych <xeenych@gmail.com>
2023-10-22 17:59:38 +0300

    1

--- stm32_hal/stm32f4xx/spi/spi.hpp
+++ stm32_hal/stm32f4xx/spi/spi.hpp
@@ -17,6 +17,26 @@ class spi_t : public spi_itf {
         assert(HAL_OK == status);
     };
 
+	void read(const std::span<uint8_t>& data)
+    {
+        auto status = HAL_SPI_Receive(&_hspi, data.data(), data.size_bytes(), HAL_MAX_DELAY);
+        assert(HAL_OK == status);
+    };
+
+    void write(uint8_t byte)
+    {
+        auto status = HAL_SPI_Transmit(&_hspi, &byte, 1, HAL_MAX_DELAY);
+        assert(HAL_OK == status);
+    }
+
+    uint8_t readwrite(uint8_t byte)
+    {
+        uint8_t rcv;
+        auto status = HAL_SPI_TransmitReceive(&_hspi, &byte, &rcv, 1, HAL_MAX_DELAY);
+        assert(HAL_OK == status);
+        return rcv;
+    }
+
   private:
     SPI_HandleTypeDef& _hspi;
 };
