42423bbc85348fd473847f39cd80430dafbf1e04
Xeenych <xeenych@gmail.com>
2023-10-22 23:10:26 +0300

    works

--- stm32_hal/stm32f4xx/adc/adc_channel_it.cpp
+++ stm32_hal/stm32f4xx/adc/adc_channel_it.cpp
@@ -1,4 +1,4 @@
-#define LOG_ENABLE 1
+#define LOG_ENABLE 0
 
 #include "adc_channel_it.hpp"
 
@@ -10,6 +10,7 @@ using namespace ZOQ::stm32_hal::stm32f4xx::adc;
 
 extern "C" void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* hadc)
 {
+    LOG_DBG("HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* hadc)");
     adc_channel_it_t::_instance->on_measure_end();
 }
 
@@ -24,6 +25,7 @@ void adc_channel_it_t::on_measure_end() const
 
 void adc_channel_it_t::measure() const
 {
+    LOG_DBG("adc_channel_it_t::measure()");
     ADC_ChannelConfTypeDef sConfig = {.Channel = _channel, .Rank = 1, .SamplingTime = _sample_time, .Offset = 0};
     {
         auto status = HAL_ADC_ConfigChannel(&_handle, &sConfig);
@@ -34,6 +36,7 @@ void adc_channel_it_t::measure() const
         auto status = HAL_ADC_Start_IT(&_handle);
         assert(HAL_OK == status);
     }
+    LOG_DBG("adc_channel_it_t::measure() END");
 }
 
 }  // namespace ZOQ::stm32_hal::stm32f4xx::adc
