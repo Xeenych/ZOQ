68e37507bd6aacfa5a0f333bd77bb3e5901506e8
Alexey Tsvetkov <avtsvetkov@sberdevices.ru>
2024-11-29 21:35:56 +0300

    1

--- stm32_hal/stm32f4xx/timers/timer_event.hpp
+++ stm32_hal/stm32f4xx/timers/timer_event.hpp
@@ -9,9 +9,9 @@ namespace ZOQ::stm32_hal::stm32f4xx::timers {
 
 using namespace ZOQ;
 
-class oneshot_timer_event_t {
+class oneshot_event_t {
   public:
-    oneshot_timer_event_t(scheduler_t& s, const callback_t& cb) : _e{s, cb} {}
+    oneshot_event_t(scheduler_t& s, const callback_t& cb) : _e{s, cb} {}
     void start(uint32_t period) { _e.arm(period, 0); };
     void stop() { _e.disarm(); };
     bool is_armed() const { return _e.armed(); }
@@ -20,9 +20,9 @@ class oneshot_timer_event_t {
     event_t _e;
 };
 
-class periodic_timer_event_t {
+class periodic_event_t {
   public:
-    periodic_timer_event_t(scheduler_t& s, const callback_t& cb) : _e{s, cb} {}
+    periodic_event_t(scheduler_t& s, const callback_t& cb) : _e{s, cb} {}
     void start(uint32_t period) { _e.arm(0, period); }
     void stop() { _e.disarm(); }
     bool is_armed() const { return _e.armed(); }
