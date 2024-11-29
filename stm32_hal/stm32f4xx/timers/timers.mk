c3c296a94406398d37a4a2fa53f38387efc31412
Alexey Tsvetkov <avtsvetkov@sberdevices.ru>
2024-11-29 23:03:39 +0300

    remove

--- stm32_hal/stm32f4xx/timers/timers.mk
+++ stm32_hal/stm32f4xx/timers/timers.mk
@@ -1,4 +1,3 @@
-CXX_SOURCES += ${ZOQ_ROOT}/stm32_hal/stm32f4xx/timers/oneshot_timer.cpp
 CXX_SOURCES += ${ZOQ_ROOT}/stm32_hal/stm32f4xx/timers/general_purpose_timer.cpp
 CXX_SOURCES += ${ZOQ_ROOT}/stm32_hal/stm32f4xx/timers/scheduler.cpp
 CXX_SOURCES += ${ZOQ_ROOT}/stm32_hal/stm32f4xx/timers/event.cpp
