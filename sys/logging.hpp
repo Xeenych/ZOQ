593c5f5a33a0c71532d1f3f09fef445e747b9df6
Xeenych <xeenych@gmail.com>
2024-12-22 18:53:10 +0300

    LOG_D

--- sys/logging.hpp
+++ sys/logging.hpp
@@ -1,8 +1,12 @@
 #pragma once
 #include "log_print.hpp"
 
-#define LOG_D(fmt, ...)                         \
-    do {                                        \
-        if (1)                                  \
-            log_print(fmt "\n", ##__VA_ARGS__); \
+#ifdef LOG_ENABLE
+#define LOG_D(fmt, ...)                                             \
+    do {                                                            \
+        if (LOG_ENABLE)                                             \
+            log_print("%-30s: " fmt "\n", __FILE__, ##__VA_ARGS__); \
     } while (0)
+#else
+#define LOG_D(fmt, ...)
+#endif
