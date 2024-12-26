98c6c785f25c54375b328f9455091ca3b3883e4e
Xeenych <xeenych@gmail.com>
2024-12-26 22:47:28 +0300

    fix scheduler

--- scheduler/scheduler.hpp
+++ scheduler/scheduler.hpp
@@ -14,7 +14,7 @@ class scheduler_t : public scheduler_itf {
     constexpr scheduler_t(it_timer_itf& t) : _t{t} {
         _t.set_callback({+[](scheduler_t* arg) { arg->tick(); }, this});
     }
-    // Обрабатываем ивенты за два прохода, чтобы исключить ситуацию, когда внутри ивента заряжеается другой ивент. Тогда
+    // Обрабатываем ивенты за три прохода, чтобы исключить ситуацию, когда внутри ивента заряжеается другой ивент. Тогда
     // его счетчик может быть уменьшен на 1 в этом же цикле
     void tick() {
         // За первый проход уменьшаем счетчики
@@ -24,10 +24,15 @@ class scheduler_t : public scheduler_itf {
 
         // Во второй проход срабатываем на истекшие ивенты
         for (event_t* t = _head; t != nullptr; t = t->next()) {
-            if (t->is_expiring()) {
+            if (t->is_expiring())
                 t->execute();
+        }
+
+        // В третий проход перезагружаем таймеры
+        // Если во втормо проходе заново стартует истекший oneshot таймер, то он будет перезагруженнуем и не сработает
+        for (event_t* t = _head; t != nullptr; t = t->next()) {
+            if (t->is_expiring())
                 t->reload();
-            }
         }
     }
 
