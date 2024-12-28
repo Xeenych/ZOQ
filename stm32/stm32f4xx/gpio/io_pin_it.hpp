66b326bbe4ef51893f082405ad1c27e7de73fd07
Xeenych <xeenych@gmail.com>
2024-12-28 08:16:17 +0300

    init pins

--- stm32/stm32f4xx/gpio/io_pin_it.hpp
+++ stm32/stm32f4xx/gpio/io_pin_it.hpp
@@ -12,7 +12,18 @@ using namespace ZOQ::itf;
 
 class io_pin_it_t final : public it_pin_itf {
   public:
-    constexpr io_pin_it_t(const pin_name_t& p) : _p{p} {}
+    // mode:
+    //    - GPIO_MODE_IT_RISING
+    //    - GPIO_MODE_IT_FALLING
+    //    - GPIO_MODE_EVT_RISING_FALLING
+    io_pin_it_t(const pin_name_t& p, uint32_t mode) : _p{p} {
+        GPIO_InitTypeDef GPIO_InitStruct = {0};
+        GPIO_InitStruct.Pin = p.pin;
+        GPIO_InitStruct.Mode = mode;
+        GPIO_InitStruct.Pull = GPIO_NOPULL;
+        GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+        HAL_GPIO_Init(p.port, &GPIO_InitStruct);
+    }
 
     constexpr void set_callback(const callback_t& cb) { _callback = cb; }
 
@@ -20,8 +31,9 @@ class io_pin_it_t final : public it_pin_itf {
     constexpr void reset() { _p.port->BSRR = (uint32_t)(_p.pin << 16U); }
     bool get() { return ((_p.port->IDR & _p.pin) != (uint32_t)GPIO_PIN_RESET); }
 
-    void EXTI_IRQHandler()
-    {
+    ~io_pin_it_t() { HAL_GPIO_DeInit(_p.port, _p.pin); }
+
+    void EXTI_IRQHandler() {
         if (!LL_EXTI_IsActiveFlag_0_31(_p.pin))
             return;
         LL_EXTI_ClearFlag_0_31(_p.pin);
--- stm32_hal/stm32f4xx/gpio/o_pin.hpp
+++ stm32_hal/stm32f4xx/gpio/o_pin.hpp
@@ -11,9 +11,23 @@ using namespace ZOQ::stm32::stm32f4xx::gpio;
 
 class o_pin_t : public itf::o_pin_itf {
   public:
-    constexpr o_pin_t(const pin_name_t& p) : _port(p.port), _pin(p.pin) {}
+    o_pin_t(const pin_name_t& p, bool state) : _port(p.port), _pin(p.pin) {
+        if (state)
+            set();
+        else
+            reset();
+
+        GPIO_InitTypeDef GPIO_InitStruct = {0};
+        GPIO_InitStruct.Pin = p.pin;
+        GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+        GPIO_InitStruct.Pull = GPIO_NOPULL;
+        GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+        HAL_GPIO_Init(p.port, &GPIO_InitStruct);
+    }
     constexpr void set() override { _port->BSRR = _pin; }
     constexpr void reset() override { _port->BSRR = (uint32_t)(_pin << 16U); }
+
+    ~o_pin_t() { HAL_GPIO_DeInit(_port, _pin); }
 
     o_pin_t(const o_pin_t&) = delete;
     o_pin_t& operator=(const o_pin_t&) = delete;
