67ded72bb35c9b702ba4700915c7e9b4377eed83
Xeenych <xeenych@gmail.com>
2025-05-31 15:48:38 +0300

    clean

--- stm32_hal/stm32f4xx/gpio/io_pin.hpp
+++ stm32_hal/stm32f4xx/gpio/io_pin.hpp
@@ -1,12 +1,11 @@
 #pragma once
 
 #include "ZOQ/itf/gpio/io_pin_itf.hpp"
-#include "pin_name.hpp"
+#include "stm32f4xx_hal.h"
 
-namespace ZOQ::stm32::stm32f4xx::gpio {
+namespace ZOQ::stm32_hal::stm32f4xx::gpio {
 
-using namespace ZOQ::itf;
-using namespace ZOQ::stm32::stm32f4xx::gpio;
+using io_pin_itf = ZOQ::itf::io_pin_itf;
 
 class io_pin_t final : public io_pin_itf {
   public:
@@ -17,14 +16,14 @@ class io_pin_t final : public io_pin_itf {
     //    - GPIO_NOPULL
     //    - GPIO_PULLUP
     //    - GPIO_PULLDOWN
-    io_pin_t(const pin_name_t& p, uint32_t mode, uint32_t pull, bool state) : _port(p.port), _pin(p.pin) {
+    io_pin_t(GPIO_TypeDef* port, uint32_t pin, uint32_t mode, uint32_t pull, bool state) : _port{port}, _pin{pin} {
         state ? set() : reset();
         GPIO_InitTypeDef GPIO_InitStruct{};
-        GPIO_InitStruct.Pin = p.pin;
+        GPIO_InitStruct.Pin = pin;
         GPIO_InitStruct.Mode = mode;
         GPIO_InitStruct.Pull = pull;
         GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-        HAL_GPIO_Init(p.port, &GPIO_InitStruct);
+        HAL_GPIO_Init(port, &GPIO_InitStruct);
     }
     void set() override { _port->BSRR = _pin; }
     void reset() override { _port->BSRR = (_pin << 16U); }
@@ -40,4 +39,4 @@ class io_pin_t final : public io_pin_itf {
     const uint32_t _pin = 0;
 };
 
-}  // namespace ZOQ::stm32::stm32f4xx::gpio
+}  // namespace ZOQ::stm32_hal::stm32f4xx::gpio
